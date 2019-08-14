ae900584b6c7150d906570c97be8ec904a4900bf
Baljeet Singh <bsinghs.007@gmail.com>
2019-08-14 16:33:29 +0530

    quotes page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule]
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+  ],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule]
 })
 export class MaterialModule {}
--- src/app/quotes/quotes.module.ts
+++ src/app/quotes/quotes.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { QuotesRoutingModule } from './quotes-routing.module';
 import { QuotesComponent } from './quotes.component';
-
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [QuotesComponent],
-  imports: [
-    CommonModule,
-    QuotesRoutingModule
-  ]
+  imports: [CommonModule, QuotesRoutingModule, MaterialModule]
 })
-export class QuotesModule { }
+export class QuotesModule {}
