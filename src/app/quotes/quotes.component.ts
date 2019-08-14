7841368439b9ad1905e10a4676dc3721f0596165
Baljeet Singh <bsinghs.007@gmail.com>
2019-08-14 17:04:23 +0530

    random avatar background

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.scss']
 })
 export class QuotesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return { background: color };
   }
-
 }
