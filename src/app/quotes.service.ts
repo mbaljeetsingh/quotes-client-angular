88fa7ebc28d8718269b699c8195c5759e56d3670
Baljeet Singh <bsinghs.007@gmail.com>
2019-08-16 16:41:02 +0530

    rxjs tap operator

--- src/app/quotes.service.ts
+++ src/app/quotes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Quote } from './quotes/quote';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ export class QuotesService {
   constructor(private http: HttpClient) {}
 
   getData(): Observable<Quote[]> {
-    return this.http.get<Quote[]>('http://localhost:3000/quotes');
+    return this.http
+      .get<Quote[]>('http://localhost:3000/quotes')
+      .pipe(tap(data => console.log('Qutoes: ', data)));
   }
 }
