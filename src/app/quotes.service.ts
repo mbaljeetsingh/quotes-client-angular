47cd96a524db03bfbacaebd67c2e346fa3515476
Baljeet Singh <bsinghs.007@gmail.com>
2019-08-16 16:47:35 +0530

    environment variables

--- src/app/quotes.service.ts
+++ src/app/quotes.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Quote } from './quotes/quote';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,7 @@ export class QuotesService {
 
   getData(): Observable<Quote[]> {
     return this.http
-      .get<Quote[]>('http://localhost:3000/quotes')
+      .get<Quote[]>(`${baseUrl}/quotes`)
       .pipe(tap(data => console.log('Qutoes: ', data)));
   }
 }
